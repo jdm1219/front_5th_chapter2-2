630e396e860eae2805253427846360f91b6386f0
domyeong <wjsehaud9413@naver.com>
2025-04-21 23:12:31 +0900

    feat: useProduct 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import { Product } from "../../types.ts";
+import { append } from "../utils/pureFunctions.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState(initialProducts);
+
+  const updateProduct = (product: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === product.id ? product : p)),
+    );
+  };
+
+  const addProduct = (product: Product) => {
+    setProducts((prevProducts) => append(prevProducts, product));
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
