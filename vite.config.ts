636a8e30f7bb360ec6530d45edb108b2e538b091
domyeong <wjsehaud9413@naver.com>
2025-04-24 21:13:31 +0900

    feat: gh-page 배포 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { loadEnv } from "vite";
+import { resolve } from "path";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
-    },
-  })
-)
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return mergeConfig(
+    defineConfig({
+      plugins: [react()],
+    }),
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "src/setupTests.ts",
+      },
+      base: env.VITE_BASE_URL,
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.refactoring.html"),
+          },
+        },
+      },
+    }),
+  );
+};
